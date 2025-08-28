962ed38b65632759589ed26e9f371a98b4b91fe2
SESA711346 <huzefa.essaji@se.com>
2025-08-28 15:46:03 +0530

    rectified error in index.js

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {l
+app.whenReady().then(() => {
   createWindow();
 
   // On OS X it's common to re-create a window in the app when the
